6fd3aef467a82ac28f97b8588f0f58de940192e6
Rhizome Networks <Rhizome Networks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-03 10:02:22 +0000

    Fix configoration page.
    git-svn-id: https://plugins.svn.wordpress.org/content-glass-button/trunk@1191698 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Options.php
@@ -0,0 +1,10 @@
+<?php
+/**
+  * Copyright © 2009-2014 Rhizome Networks LTD All rights reserved.
+  *
+  * Created by IntelliJ IDEA.
+  * User: Tomer Schilman
+  * Date: 03/07/2015
+  * Time: 13:01
+  */
+ 
